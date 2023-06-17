abd7f7e880313e33a80057ecdc267ff04026ed0a
Aegean <todddharni@gmail.com>
2023-06-17 15:57:33 -0600

    Added port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const routes = require('./routes/index.js');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // 
 app.use(express.json());
@@ -28,5 +28,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening at http://localhost:${PORT}`)
+  console.log(`Listening at ${PORT}`)
 });
